2332c18b26fe2cafc6563158d9cfa9d69c9ea996
Ken Burgett <keburgett@gmail.com>
2017-06-05 21:58:50 -0700

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def topPane
+    render_component
+  end
+end
---
+++ generate_app.sh
@@ -0,0 +1,4 @@
+# bundle update
+# rails g hyperloop:install
+# rails g hyper:component TopPane
+rails g controller Home topPane --skip-javascripts
